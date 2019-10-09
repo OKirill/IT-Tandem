c00370e6dafe53c423a8fa77a480b584deb538d6
Kyrylo Oliinyk <43280796+OKirill@users.noreply.github.com>
2019-10-09 16:25:26 +0200

    Delete views.py

--- authapp/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render, reverse
-from django.views.generic.detail import DetailView
-from .models import User
-
-
-
-class UserProfile(DetailView):
-    model = User
-    template_name = 'authapp/profile.html'
