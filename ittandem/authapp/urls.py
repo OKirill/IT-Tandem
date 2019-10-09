7d41892c288edc3bca3275fa7f62a73aff5e6885
PetrMixayloff <mpk29760@mail.ru>
2019-10-09 22:57:29 +0300

    Поменял имя

--- ittandem/authapp/urls.py
+++ ittandem/authapp/urls.py
@@ -1,11 +1,11 @@
-from django.urls import path
 import authapp.views as authapp
+from django.urls import path
 
 app_name = 'authapp'
 
 urlpatterns = [
     # path('login/', authapp.login, name='login'),
-    # path('join/', authapp.join, name='join'),
+    path('join/', authapp.register, name='register'),
     # path('<int:pk>/', authapp.UserProfile.as_view(), name='profile'),
     # path('<int:pk>/edit', authapp.edit, name='edit'),
 ]
