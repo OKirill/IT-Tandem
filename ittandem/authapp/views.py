12d3fa27d9519d9f1601d5f77251770235af0dfd
PetrMixayloff <mpk29760@mail.ru>
2019-10-13 15:58:11 +0300

    Добавил контроллеры для входа и выхода

--- ittandem/authapp/views.py
+++ ittandem/authapp/views.py
@@ -2,7 +2,31 @@ from django.shortcuts import render, HttpResponseRedirect
 from django.urls import reverse
 from django.core.mail import send_mail
 from authapp.forms import UserRegisterForm
+from authapp.forms import UserLoginForm
 from django.conf import settings
+
+
+def login(request):
+    title = 'вход'
+
+    login_form = UserLoginForm(data=request.POST)
+    if request.method == 'POST' and login_form.is_valid():
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+        if user and user.is_active:
+            auth.login(request, user)
+            return HttpResponseRedirect(reverse('main'))
+
+    context = {'title': title, 'login_form': login_form}
+    return render(request, 'authapp/login.html', context)
+
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('main'))
+
 
 def register(request):
     if request.method == 'POST':
