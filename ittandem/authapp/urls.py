90f7b1faca7253b90505b02964e05d073567c069
PetrMixayloff <mpk29760@mail.ru>
2019-10-13 15:57:13 +0300

    Добавил пути для авторизации и выхода

--- ittandem/authapp/urls.py
+++ ittandem/authapp/urls.py
@@ -4,7 +4,8 @@ from django.urls import path
 app_name = 'authapp'
 
 urlpatterns = [
-    # path('login/', authapp.login, name='login'),
+    path('login/', authapp.login, name='login'),
+    path('logout/', authapp.logout, name='logout'),
     path('join/', authapp.register, name='register'),
     # path('<int:pk>/', authapp.UserProfile.as_view(), name='profile'),
     # path('<int:pk>/edit', authapp.edit, name='edit'),
