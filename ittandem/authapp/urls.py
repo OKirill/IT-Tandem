36b4229ee7c349f9ff016709c96d8bec951e89e8
PetrMixayloff <mpk29760@mail.ru>
2019-10-13 17:33:58 +0300

    Обновил

--- ittandem/authapp/urls.py
+++ ittandem/authapp/urls.py
@@ -1,5 +1,5 @@
 import authapp.views as authapp
-from django.urls import path
+from django.urls import path, re_path
 
 app_name = 'authapp'
 
@@ -8,5 +8,6 @@ urlpatterns = [
     path('logout/', authapp.logout, name='logout'),
     path('join/', authapp.register, name='register'),
     # path('<int:pk>/', authapp.UserProfile.as_view(), name='profile'),
-    # path('<int:pk>/edit', authapp.edit, name='edit'),
+    path('<int:pk>/edit', authapp.edit, name='edit'),
+    re_path(r'^verify/(?P<email>.+)/(?P<activation_key>\w+)/$', authapp.verify, name='verify'),
 ]
